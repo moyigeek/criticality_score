b8053c968d6475bc753dd503eba83f6229b3b190
a2ure123 <2059970466@qq.com>
2024-07-31 16:52:54 +0800

    This time, some bugs related to depsdev’s api acquisition have been updated.

--- pkg/collector_depsdev/collector_depsdev.go
+++ pkg/collector_depsdev/collector_depsdev.go
@@ -5,9 +5,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/google/go-github/v63/github"
 	"golang.org/x/oauth2"
@@ -59,54 +63,96 @@ func getProjectType(owner, repo string) string {
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
+attempt:
 	// 获取仓库内容
-	_, dirContent, _, err := client.Repositories.GetContents(ctx, owner, repo, "", nil)
+	_, dirContent, resp, err := client.Repositories.GetContents(ctx, owner, repo, "", nil)
 	if err != nil {
+		if resp != nil && resp.StatusCode == http.StatusForbidden {
+			fmt.Println("Error fetching repository contents:", err)
+			body := err.Error()
+			re := regexp.MustCompile(`rate reset in (\d+)m(\d+)s`)
+			matches := re.FindStringSubmatch(body)
+			if len(matches) == 3 {
+				minutes, _ := strconv.Atoi(matches[1])
+				seconds, _ := strconv.Atoi(matches[2])
+				waitTime := time.Duration(minutes)*time.Minute + time.Duration(seconds)*time.Second
+				fmt.Printf("Rate limit exceeded. Waiting %v to retry...\n", waitTime)
+				time.Sleep(waitTime)
+				goto attempt
+			}
+		}
 		fmt.Println("Error fetching repository contents:", err)
 		return ""
 	}
 
 	// 判断项目类型
 	for _, file := range dirContent {
-		switch *file.Name {
-		case "package.json":
-			return "npm"
-		case "setup.py":
-			return "pypi"
-		case "Cargo.toml":
-			return "cargo"
-		case "pom.xml":
-			return "maven"
-		case "build.gradle":
-			return "gradle"
-		case "go.mod":
-			return "go"
+		if file.Name != nil {
+			switch *file.Name {
+			case "package.json":
+				return "npm"
+			case "setup.py":
+				return "pypi"
+			case "Cargo.toml":
+				return "cargo"
+			case "pom.xml":
+				return "maven"
+			case "build.gradle":
+				return "gradle"
+			case "go.mod":
+				return "go"
+			}
 		}
 	}
 	return ""
 }
 
+type VersionInfo struct {
+	VersionKey struct {
+		Version string `json:"version"`
+	} `json:"versionKey"`
+	PublishedAt time.Time `json:"publishedAt"`
+}
+
+type PackageInfo struct {
+	Versions []VersionInfo `json:"versions"`
+}
+
 func getLatestVersion(owner, repo, projectType string) string {
 	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GITHUB_AUTH_TOKEN")},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
 
-	// 获取最新的发布版本
-	release, _, err := client.Repositories.GetLatestRelease(ctx, owner, repo)
+	// 构造请求URL
+	url := fmt.Sprintf("https://api.deps.dev/v3alpha/systems/%s/packages/%s", projectType, repo)
+
+	// 发起HTTP GET请求
+	req, _ := http.NewRequest("GET", url, nil)
+	client := &http.Client{}
+	resp, err := client.Do(req.WithContext(ctx))
 	if err != nil {
-		fmt.Println("Error fetching latest release:", err)
+		fmt.Println("Error fetching package information:", err)
 		return ""
 	}
-	return release.GetTagName()
+	defer resp.Body.Close()
+
+	// 解析响应体
+	body, _ := io.ReadAll(resp.Body)
+	var result PackageInfo
+	json.Unmarshal(body, &result)
+
+	// 寻找最新版本
+	var latestVersion string
+	var latestDate time.Time
+	for _, version := range result.Versions {
+		if version.PublishedAt.After(latestDate) {
+			latestDate = version.PublishedAt
+			latestVersion = version.VersionKey.Version
+		}
+	}
+
+	return latestVersion
 }
 
 func queryDepsDev(projectType, projectName, version, outputFile string) {
-	if strings.HasPrefix(version, "v") {
-		version = version[1:]
-	}
 	url := fmt.Sprintf("https://api.deps.dev/v3alpha/systems/%s/packages/%s/versions/%s:dependents", projectType, projectName, version)
 	resp, err := http.Get(url)
 	if err != nil {
@@ -138,3 +184,4 @@ func queryDepsDev(projectType, projectName, version, outputFile string) {
 	fmt.Fprintf(file, "Dependent Count: %d, Direct Dependent Count: %d, Indirect Dependent Count: %d\n",
 		info.DependentCount, info.DirectDependentCount, info.IndirectDependentCount)
 }
+
