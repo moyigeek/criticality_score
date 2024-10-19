c5703d69f988595db01052062be134377bb58360
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2024-10-20 00:55:49 +0800

    fix(collector_git): avoid exit when failed

--- pkg/collector_git/internal/utils/utils.go
+++ pkg/collector_git/internal/utils/utils.go
@@ -33,7 +33,7 @@ func CheckIfError(err error) {
 	}
 	// logger.Warnf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
 	fmt.Printf("\x1b[31;1m%s\x1b[0m\n", fmt.Sprintf("error: %s", err))
-	os.Exit(1)
+	panic(err)
 }
 
 // Info should be used to describe the example commands that are about to run.
