71d76e26335fb246ceaffc6691ed3524b125c3af
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2024-11-12 16:51:20 +0800

    feat(workflow): update workflow

--- workflow/flow.mk
+++ workflow/flow.mk
@@ -11,7 +11,7 @@ ifeq ($(wildcard $(CFG_FILE)),)
 $(error $(CFG_FILE) not found, use CFG_FILE=... to specify the configuration file)
 endif
 
-calc_score.rec: update_dependents.rec update_git_metrics.rec update_depsdev.rec
+calc_score.rec: update_dependents.rec update_git_metrics.rec update_git_metrics_partial.rec update_depsdev.rec
 	# Calculate the score
 	echo "* Calculating the score..."
 	$(APP_BIN)/gen_scores -config $(CFG_FILE)
@@ -22,26 +22,43 @@ update_dependents.rec: package_updated.src union_gitlink.rec
 	echo "* Updating the dependents..."
 	$(APP_BIN)/show_distpkg_deps -config $(CFG_FILE) -type archlinux
 	$(APP_BIN)/show_distpkg_deps -config $(CFG_FILE) -type debian
+	$(APP_BIN)/show_distpkg_deps -config $(CFG_FILE) -type nix
+	$(APP_BIN)/show_distpkg_deps -config $(CFG_FILE) -type homebrew
 	# $(APP_BIN)/show_distpkg_deps -config $(CFG_FILE) nix
 	touch $@
 
-update_git_metrics.rec: git_updated.src union_gitlink.rec
+update_git_metrics_partial.rec: union_gitlink.rec
 	# Update the Git metrics
 	echo "* Updating the Git metrics..."
 	$(APP_BIN)/update_git_metrics -config $(CFG_FILE) -storage $(STORAGE_DIR)
 
 	touch $@
 
-update_depsdev.rec: depsdev_updated.src union_gitlink.rec update_git_metrics.rec
+update_git_metrics.rec: git_updated.src 
+	# Update the Git metrics
+	echo "* Updating the Git metrics..."
+	$(APP_BIN)/update_git_metrics -config $(CFG_FILE) -storage $(STORAGE_DIR) -force-update-all
+
+	touch $@
+
+
+update_depsdev.rec: depsdev_updated.src update_git_metrics.rec update_git_metrics_partial.rec
 	# Update from deps.dev
 	echo "* Updating from deps.dev..."
 	$(APP_BIN)/show_depsdev_deps -config $(CFG_FILE)
 	touch $@
 
 
-union_gitlink.rec: gitlink_updated.src
+union_gitlink.rec: gitlink_updated.src enumerate_github.rec
 	# Union the Git links
 	echo "* Unioning the Git links..."
 	$(APP_BIN)/gitmetricsync -config $(CFG_FILE)
 	touch $@
+
+enumerate_github.rec: github_updated.src
+	# Enumerate the GitHub repositories
+	echo "* Enumerating the GitHub repositories..."
+	$(APP_BIN)/enumerate_github -config $(CFG_FILE) -min-stars 50 -out /dev/null -workers 20
+	touch $@
+
 
