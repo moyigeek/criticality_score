20f517756b0f7e486e849425294e8e9e65af2fa7
a2ure123 <2059970466@qq.com>
2024-12-18 22:07:40 +0800

    feat: This time, we have updated the content about using large models to judge gitlink and industry information.

--- cmd/home2git/main.go
+++ cmd/home2git/main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"flag"
+	"strings"
 	"github.com/HUSTSecLab/criticality_score/pkg/home2git"
 )
 var flagConfigPath = flag.String("config", "config.json", "path to the config file")
 var choice = flag.String("choice", "", "which function to run etc. home2git, identifyCountry, identifyIndustry")
-var outputCSV = flag.String("out", "output.csv", "output file")
+var repo = flag.String("repo", "", "input repo name")
 var url = flag.String("url", "", "input LLM url")
+var batchSize = flag.Int("batch", 1000, "batch size")
+
 func main() {
 	flag.Parse()
 	if *choice == "home2git" {
-		home2git.Home2git(*flagConfigPath, *outputCSV, *url)
+		repolist := strings.Split(*repo, ",")
+		home2git.Home2git(*flagConfigPath, repolist, *url, *batchSize)
 	}
-	if  *choice == "identifyCountry" {
+	if  *choice == "identifyIndustry" {
+		home2git.IndustryID(*flagConfigPath, *url, *batchSize)
 	}
 }
 
