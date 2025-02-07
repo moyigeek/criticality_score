39debc954965f215a49861722868296095ba7656
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2025-02-07 14:47:11 +0800

    feat(repository): all gitlink cache

--- pkg/storage/repository/all_gitlinks.go
+++ pkg/storage/repository/all_gitlinks.go
@@ -9,10 +9,47 @@ import (
 type AllGitLinkRepository interface {
 	/** QUERY **/
 	Query() (iter.Seq[string], error)
+	QueryCache() (iter.Seq[string], error)
+	MakeCache() error
 }
 
 type allGitLinkRepository struct {
 	ctx storage.AppDatabaseContext
+}
+
+// MakeCache implements AllGitLinkRepository.
+func (a *allGitLinkRepository) MakeCache() error {
+	_, err := a.ctx.Exec(`DROP TABLE IF EXISTS all_gitlinks_cache;
+	CREATE TABLE all_gitlinks_cache AS SELECT * FROM all_gitlinks;
+	`)
+	return err
+}
+
+// QueryCache implements AllGitLinkRepository.
+func (a *allGitLinkRepository) QueryCache() (iter.Seq[string], error) {
+	rows, err := a.ctx.Query("SELECT git_link FROM all_gitlinks_cache")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return func(yield func(string) bool) {
+		defer rows.Close()
+		for rows.Next() {
+			var link *string
+			err := rows.Scan(&link)
+			if err != nil {
+				return
+			}
+			if link == nil {
+				continue
+			}
+
+			if !yield(*link) {
+				return
+			}
+		}
+	}, nil
 }
 
 var _ AllGitLinkRepository = (*allGitLinkRepository)(nil)
@@ -30,6 +67,7 @@ func (a *allGitLinkRepository) Query() (iter.Seq[string], error) {
 	}
 
 	return func(yield func(string) bool) {
+		defer rows.Close()
 		for rows.Next() {
 			var link *string
 			err := rows.Scan(&link)
